acf1f26451aa38d741a4a6ba803c51f003381cea
Jason Marshal <A0008282@wits.ac.za>
2022-09-02 10:22:35 +0200

    fixing error in norm kern

---
+++ step3_fitNormKern150-nimble.R
@@ -0,0 +1,104 @@
+# TENR Zebra SCR analysis - Process data for grid analysis from search path
+# Set for parallel running on cluster
+
+## set up workspace
+library(nimble)
+
+# load data
+load('spacing150.RData')
+#my.data <- c(zebData[[1]], zebData[[2]])
+
+my.data <- zebData[[1]]
+
+my.const <- zebData[[2]]
+
+
+s.init <- zebData[[3]]
+w.init <- zebData[[4]]
+#my.inits <- function(){list(beta0 = runif(1, -1, 1),
+#                            beta1 = runif(1, -1, 1),
+#                            lsigma = runif(1, -1, 0.5),
+#                            s = s.init, w = w.init)}
+
+my.inits <- list(beta0 = runif(1, -25, 25),
+                 beta1 = runif(1, -25, 25),
+                 lsigma = runif(1, -5, 5),
+                 s = s.init, w = w.init)
+
+my.params <- zebData[[5]]
+
+## model specification in BUGS
+#cat('
+#model{
+
+my.code <- nimbleCode({
+
+# Priors
+beta0 ~ dunif(-25, 25)
+beta1 ~ dunif(-25, 25)
+lsigma ~ dunif(-5, 5)
+sigma <- exp(lsigma)
+tau <- 1/(sigma*sigma)
+psi ~ dunif(0, 1)
+
+# Likelihood
+for(i in 1:M){ # Loop over individuals
+  w[i] ~ dbern(psi)
+  s[i,1] ~ dunif(Xl, Xu)
+  s[i,2] ~ dunif(Yl, Yu)
+  for(k in 1:K){ # Loop over temporal replicates
+    u[i,k] ~ dnorm(s[i,1], tau)
+    v[i,k] ~ dnorm(s[i,2], tau)
+    for(j in 1:J){ # Loop over each point defining line segments
+      d[i,k,j] <- pow(pow(u[i,k] - X[j,1,k], 2) + pow(v[i,k] - X[j,2,k], 2), 0.5)
+      #h[i,k,j] <- exp(-beta0 - beta1*d[i,k,j]) # Gompertz
+      #h[i,k,j] <- exp(-beta0 - beta1*d[i,k,j]*d[i,k,j]) # SqDist
+      h[i,k,j] <- exp(-log(1 - (exp(beta0)/(1+exp(beta0)))*exp(beta1*pow(d[i,k,j]), 2))) # NormKern
+      #h[i,k,j] <- exp(beta0 + beta1*log(d[i,k,j])) # Weibull
+
+      # Bayesian p-value
+      mu[i,k,j] <- w[i]*p[i,k]
+      mu2[i,k,j] <- mu[i,k,j]*K
+    }
+    H[i,k] <- sum(h[i,k,1:J])
+    p[i,k] <- w[i]*(1 - exp(-H[i,k]))
+    y[i,k] ~ dbern(p[i,k])
+    ynew[i,k] ~ dbern(p[i,k])
+  }
+  # Bayesian p-value GOF: discrepancy across individuals
+  expected.t2[i] <- sum(mu2[i,,])
+  nsum.t2[i] <- sum(y[i,])
+  nsumnew.t2[i] <- sum(ynew[i,])
+  err.t2[i] <- pow(pow(nsum.t2[i], 0.5) - pow(expected.t2[i], 0.5), 2)
+  errnew.t2[i] <- pow(pow(nsumnew.t2[i], 0.5) - pow(expected.t2[i], 0.5), 2)
+}
+
+# Derived quantity
+N <- sum(w[])
+T1obs <- sum(err.t2[])
+T1new <- sum(errnew.t2[])
+})
+#', file = 'modelnormKern.txt')
+
+# MCMC settings
+nc <- 1#3
+ni <- 10#000
+nb <- 5#000
+nt <- 1#0
+
+(start.time <- Sys.time())
+#out <- jags(data = my.data, inits = my.inits, parameters.to.save = my.params,
+#            n.iter = ni, n.burnin = nb, n.thin = nt, n.chains = nc,
+#            model.file = 'modelnormKern.txt', parallel = T)
+
+out <- nimbleMCMC(code = my.code, data = my.data, constants = my.const,
+                  inits = my.inits, monitors = my.params,
+                  thin = nt, niter = ni, nburnin = nb, nchains = nc,
+                  dimensions = list(mu2 = c(my.const$M, my.const$K, my.const$J),
+                                    ynew = c(my.const$M, my.const$K),
+                                    err.t2 = my.const$M, errnew.t2 = my.const$M))
+
+(end.time <- Sys.time())
+print(difftime(end.time, start.time, units = 'hours'), 3)
+
+save(out, file = 'normKern150Out.RData')
