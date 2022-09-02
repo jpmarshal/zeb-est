daf50ffa242f98ac94e92c76819c0ab854bb09fd
Jason Marshal <A0008282@wits.ac.za>
2022-09-02 10:16:30 +0200

    set up for cluster

---
+++ sqDist125.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=stampede
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --job-name=step3_fitSqDist125.R
+#SBATCH --output=/home-mscluster/jmarshal/output.txt
+#SBATCH --workdir=/home-mscluster/jmarshal/
+
+R CMD BATCH step3_fitSqDist125.R
